e50165d243454ec0a3d758776d2905e06825a6f5
joycemathieu <j.matthieu@cci-formation.nc>
2021-04-28 20:24:01 +1100

    update

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const list = document.getElementById('list');
 
 // function show API
 function showData(employees) {
-  employees.forEach((employe, index) => {
-    const listItem = createMyLi(employe, index);
+  employees.forEach((employe) => {
+    const listItem = createMyLi(employe);
     list.appendChild(listItem);
   });
 }
@@ -11,25 +11,65 @@ function showData(employees) {
 // function creation list
 function createMyLi(employe) {
     const listItem = document.createElement('li');
-    const btnView = createBtns('view btn btn-primary', 'View more');
+    const btnViewMore = linkBtns('btn btn-primary', 'View more', 'modal', '#modal', 'button');
     const name = createColumn('span', 'name', employe.name);
     const lastName = createColumn('span', 'lastName', employe.last_name);
     listItem.setAttribute('id', employe.id);
 
     listItem.appendChild(name);
     listItem.appendChild(lastName);
-    listItem.appendChild(btnView);
+    listItem.appendChild(btnViewMore);
   
-    btnView.addEventListener('click', function(event) {
+    btnViewMore.addEventListener('click', function(event) {
       viewMore(employe);
+      const modalContent = document.getElementsByClassName('modal-content')[0];
+      console.log(modalContent);
+      const editBtn = btnModal('btn btn-primary', 'Edit', '#modal2', 'modal', 'modal');
+      const deleteBtn = btnModal('btn btn-danger', 'Delete', '#modal2', 'modal', 'modal');
+      const name = createColumn('span', 'name', employe.name);
+      const lastName = createColumn('span', 'lastName', employe.last_name);
+      const job = createColumn('span', 'job', employe.job_title);
+      const email = createColumn('span', 'email', employe.email);
+    
+      modalContent.appendChild(name);
+      modalContent.appendChild(lastName);
+      modalContent.appendChild(job);
+      modalContent.appendChild(email);
+      modalContent.appendChild(editBtn);
+      modalContent.appendChild(deleteBtn);
     });
-  
+ 
     return listItem;
+}
+
+  //<button class="btn btn-primary" data-bs-target="#modal2" data-bs-toggle="modal" data-bs-dismiss="modal">Open #modal2</button>
+function btnModal(className, text, dataTarget, dataToggle, dataDismiss) {
+  const btnModalElement = document.createElement('button');
+  btnModalElement.innerHTML = text;
+  btnModalElement.setAttribute('data-bs-target', dataTarget);
+  btnModalElement.setAttribute('data-bs-toggle', dataToggle);
+  btnModalElement.setAttribute('data-bs-dismiss', dataDismiss);
+  btnModalElement.setAttribute('class', className);
+  
+  return btnModalElement;
 }
 
 function viewMore(employe){
   console.log(employe);
   
+
+
+}
+//<a class="btn btn-primary" data-bs-toggle="modal" href="#modal" role="button">Open #modal</a>
+function linkBtns(className, text, dataToggle, link, role) {
+  const linkBtnElement = document.createElement('a');
+  linkBtnElement.innerHTML = text;
+  linkBtnElement.setAttribute('data-bs-toggle', dataToggle);
+  linkBtnElement.setAttribute('class', className);
+  linkBtnElement.setAttribute('href', link);
+  linkBtnElement.setAttribute('role', role);
+
+  return linkBtnElement;
 }
 
 function createBtns(className, text) {
